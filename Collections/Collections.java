04a8456fdc065cd74c2ccee7b0225570091c5be6
Anurag <anuragnegi2704@gmail.com>
2024-07-27 10:20:17 +0530

    try

---
+++ Collections/Collections.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Collections
+{
+public static void main(String []args)
+{
+    ArrayList <String> l1= new ArrayList();
+    l1.add("Anurag");
+    l1.add("Rohit");
+    l1.add("Zaid");
+    System.out.println(l1);
+    
+}
+}
